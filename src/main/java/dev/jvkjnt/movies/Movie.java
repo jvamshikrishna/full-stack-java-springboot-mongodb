cd8bc7ddca3d50aa2c37b23519186f2229016823
jvamshikrishna <jvamshikrishna1997@gmail.com>
2023-08-01 15:46:34 -0500

    fullstack java  spring mongo db project

--- src/main/java/dev/jvkjnt/movies/Movie.java
+++ src/main/java/dev/jvkjnt/movies/Movie.java
@@ -1,4 +1,21 @@
 package dev.jvkjnt.movies;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
 public class Movie {
+
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerlink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+
+
 }
